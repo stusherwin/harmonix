d6dd31a2fd7ede4f9e327d92422b8343f63475be
Stu Sherwin <stu.sherwin@gmail.com>
2017-08-04 00:21:38 +0100

    Initial commit

---
+++ app/Main.hs
@@ -0,0 +1,205 @@
+module Main where
+
+import Music (Note (..), Scale (..), ScaleType (..), notes, inScale)
+import Data.Char (chr)
+import System.Console.ANSI
+import Control.Monad (when)
+import Control.Applicative (ZipList (..), (<$>), (<*>))
+
+f = [ "│  │░░░░││░░░░│ │ │░░░░││░░░░││░░░│ │  │░░░░││░░░░│ │ │░░░░││░░░░││░░░│ │"
+    , "│  │░C#░││░Eb░│ │ │░F#░││░Ab░││░Bb│ │  │░C#░││░Eb░│ │ │░F#░││░Ab░││░Bb│ │"
+    , "│  └──┬─┘└─┬──┘ │ └──┬─┘└──┬─┘└─┬─┘ │  └──┬─┘└─┬──┘ │ └──┬─┘└──┬─┘└─┬─┘ │"
+    , "│ C   │ D  │  E │F   │ G   │ A  │  B│ C   │ D  │  E │F   │ G   │ A  │  B│"
+    , "└─────┴────┴────┴────┴─────┴────┴───┴─────┴────┴────┴────┴─────┴────┴───┘"
+    ]
+
+pad :: Int -> String -> String
+pad width str = str ++ (take (subtract (length str) width) $ repeat ' ')
+
+replace :: Char -> Char -> String -> String
+replace a b = map (\c -> if c == a then b else c)
+
+type Key = (Note, Bool)
+type Pos = (Int, Int)
+
+keys :: [Key]                     --  C    C#   D    Eb   E    F    F#   G    Ab   A    Bb   B
+keys = zip (take 24 $ cycle notes) $ True:True:True:True:True:True:True:True:True:True:True:False:True:(repeat False)
+
+downArrow :: String
+downArrow = [chr 0x19]
+upArrow :: String
+upArrow = [chr 0x18]
+
+data KeyShape = CFKey | DAKey | EBKey | GKey | BlkKey
+data Command = P Col String | Pn Col String | Mv Int Int | Jmp Pos
+
+data Col = Col ColorIntensity Color ColorIntensity Color
+
+defaultColor = (Col Vivid White Dull Black)
+
+setColor :: Col -> IO ()
+setColor (Col fi fc bi bc) = setSGR [ SetConsoleIntensity NormalIntensity
+                                    , SetColor Foreground fi fc
+                                    , SetColor Background bi bc
+                                    ]
+
+out :: [Command] -> IO ()
+out = mapM_ out' where
+  out' :: Command -> IO ()
+  out' (P col str) = putStrMask '%' col (replace ' ' '%' str)
+  out' (Pn col str) = putStrMask '%' col (replace ' ' '%' str) >> cursorBackward (length str) >> cursorDown 1
+  out' (Mv x y) = cursorForward x >> cursorDown y
+  out' (Jmp (x, y)) = setCursorPosition y x
+
+  putStrMask :: Char -> Col -> String -> IO ()
+  putStrMask _ _ [] = return ()
+  putStrMask mask col@(Col _ _ bi bc) str = do
+    setColor (Col bi bc bi bc) >> putStr masked 
+    setColor col               >> putStr text
+    putStrMask mask col rest where
+      (masked, str') = span (== mask) str
+      (text, rest) = break (== mask) str'
+
+drawKeys :: Pos -> [Key] -> IO ()
+drawKeys (x0, y0) ks = do
+  sequence_ $ getZipList $ drawKey <$> ZipList ks
+                                   <*> ZipList [(x0 + i, y0) | i <- [0, 3 ..]]
+                                   <*> ZipList (True : repeat False)
+  setColor (Col Vivid White Dull Black)
+  setCursorPosition (y0 + 7) x0 where
+    red = (Col Dull Black Vivid Red)
+    white = (Col Dull Black Vivid White)
+    darkRed = (Col Vivid White Dull Red)
+    black = (Col Vivid White Dull Black)
+
+    drawKey :: Key -> Pos -> Bool -> IO ()
+    drawKey (C, pr) p isFirst = do
+      let c = if pr then red else white
+      drawKeyShape CFKey "C" p c
+      when isFirst (out $ Jmp p : (take 7 $ repeat $ Pn c " "))
+    drawKey (Cs, pr) p _ = drawKeyShape BlkKey "C#" p $ if pr then darkRed else black
+    drawKey (D, pr) p _  = drawKeyShape DAKey  "D"  p $ if pr then red else white
+    drawKey (Eb, pr) p _ = drawKeyShape BlkKey "Eb" p $ if pr then darkRed else black
+    drawKey (E, pr) p _  = drawKeyShape EBKey  "E"  p $ if pr then red else white
+    drawKey (F, pr) p _  = drawKeyShape CFKey  "F"  p $ if pr then red else white
+    drawKey (Fs, pr) p _ = drawKeyShape BlkKey "F#" p $ if pr then darkRed else black
+    drawKey (G, pr) p _  = drawKeyShape GKey   "G"  p $ if pr then red else white
+    drawKey (Ab, pr) p _ = drawKeyShape BlkKey "Ab" p $ if pr then darkRed else black
+    drawKey (A, pr) p _  = drawKeyShape DAKey  "A"  p $ if pr then red else white
+    drawKey (Bb, pr) p _ = drawKeyShape BlkKey "Bb" p $ if pr then darkRed else black
+    drawKey (B, pr) p _  = drawKeyShape EBKey  "B"  p $ if pr then red else white
+
+    drawKeyShape :: KeyShape -> String -> Pos -> Col -> IO ()
+    drawKeyShape CFKey n p c = out $ Jmp p
+                                : Pn c "│  "
+                                : Pn c "│  "
+                                : Pn c "│  "
+                                : Pn c "│  "
+                                : Pn c "│    "
+                                : Pn c "│    "
+                                : Pn c "│    "
+                                : Mv 1 (-2) : P c n
+                                : []
+
+    drawKeyShape DAKey n p c = out $ Jmp p : Mv 1 0
+                                  : Pn c   "  "
+                                  : Pn c   "  "
+                                  : Pn c   "  "
+                                  : Pn c   "  " : Mv (-2) 0
+                                  : Pn c "│    "
+                                  : Pn c "│ D  "
+                                  : Pn c "│    "
+                                  : Mv 2 (-2) : P c n
+                                  : []
+
+    drawKeyShape EBKey n p c = out $ Jmp p : Mv 1 0
+                                : Pn c    "  "
+                                : Pn c    "  "
+                                : Pn c    "  "
+                                : Pn c    "  " : Mv (-3) 0
+                                : Pn c "│    "
+                                : Pn c "│    "
+                                : Pn c "│    "
+                                : Mv 3 (-2) : P c n
+                                : []
+
+    drawKeyShape GKey n p c = out $ Jmp p : Mv 1 0
+                                : Pn c   "  "
+                                : Pn c   "  "
+                                : Pn c   "  "
+                                : Pn c   "  " : Mv (-2) 0
+                                : Pn c "│     "
+                                : Pn c "│     "
+                                : Pn c "│     "
+                                : Mv 2 (-2) : P c n
+                                : []
+
+    drawKeyShape BlkKey n p c = out $ Jmp p : Pn c "    "
+                                  : Pn c "    "
+                                  : Pn c " Eb "
+                                  : Pn c "    "
+                                  : Mv 1 (-2) : P c n 
+                                  : []
+
+partOfScale :: [Note] -> Scale -> [(Note, Bool)]
+partOfScale ns s = map (\n -> (n, inScale s n)) ns
+
+data SharedScaleNote = SSN { note :: Note 
+                           , inPrev :: Bool
+                           , inThis :: Bool
+                           , inNext :: Bool
+                           }
+
+findSharedNotes :: [Note] -> Maybe Scale -> Scale -> Maybe Scale -> [SharedScaleNote]
+findSharedNotes ns prev this next = map (\n -> SSN { note = n
+                                                   , inPrev = maybe False (flip inScale n) prev
+                                                   , inThis = inScale this n
+                                                   , inNext = maybe False (flip inScale n) next
+                                                   }) ns
+
+markScaleSpan :: Scale -> [Note] -> [Bool]
+markScaleSpan (Scale root _) = reverse . foldl markNote [] where
+  markNote :: [Bool] -> Note -> [Bool]
+  markNote [] n = [n == root]
+  markNote (marked:ms) n | n == root = (not marked):marked:ms
+                         | otherwise = marked:marked:ms
+
+displaySharedNotes :: Pos -> [Note] -> Maybe Scale -> Scale -> Maybe Scale -> IO ()
+displaySharedNotes (x, y) ns prev this next = do
+  setCursorPosition y x
+  sequence_ $ map displayNote $ sharedNotes
+  setCursorPosition (y + 1) x
+  sequence_ $ map displayArrow $ sharedNotes where
+    sharedNotes = zip (findSharedNotes ns prev this next) (markScaleSpan this ns)
+  
+    displayNote :: (SharedScaleNote, Bool) -> IO ()
+    displayNote (ssn, marked) = do
+      setNoteColor (ssn, marked)
+      putStr (pad 3 $ show $ note ssn)
+      setColor defaultColor
+  
+    displayArrow :: (SharedScaleNote, Bool) -> IO ()
+    displayArrow (ssn, _) = do
+      setColor (Col Dull Red Dull Black)
+      putStr (pad 3 $ if (inThis ssn && inNext ssn) then downArrow else "")
+      setColor defaultColor
+  
+    setNoteColor :: (SharedScaleNote, Bool) -> IO ()
+    setNoteColor (ssn, marked) = case (inPrev ssn, inThis ssn, inNext ssn, marked) of
+      (_, False, _, False)        -> setColor (Col Dull Black  Dull Black)
+      (False, True, False, False) -> setColor (Col Vivid White Dull Black)
+      (_, False, _, True)         -> setColor (Col Dull Red    Dull Red)
+      (False, True, False, True)  -> setColor (Col Vivid White Dull Red)
+      (True, True, _, False)      -> setColor (Col Vivid Red   Dull Black)
+      (_, True, True, False)      -> setColor (Col Vivid Red   Dull Black)
+      (True, True, _, True)       -> setColor (Col Vivid Red   Dull Red)
+      (_, True, True, True)       -> setColor (Col Vivid Red   Dull Red)
+
+main :: IO ()
+main = do
+  setSGR [Reset] >> clearScreen >> setCursorPosition 0 0
+  drawKeys (1, 1) keys
+  displaySharedNotes (2, 9) (take 24 $ cycle notes) Nothing (Scale G Minor) (Just $ Scale A Diminished)
+  displaySharedNotes (2, 11) (take 24 $ cycle notes) (Just $ Scale G Minor) (Scale A Diminished) (Just $ Scale Bb Major)
+  displaySharedNotes (2, 13) (take 24 $ cycle notes) (Just $ Scale A Diminished) (Scale Bb Major) Nothing
+  setCursorPosition 15 0
---
+++ src/Music.hs
@@ -0,0 +1,171 @@
+module Music (
+  Note (..), Chord (..), ChordType (..), Scale (..), ScalePosition (..), ScaleType (..),
+  notes,
+  scales,
+  chordTypes,
+  scaleNotes,
+  chordNotes,
+  chordsForNotes,
+  scalesForChord,
+  scalesForProgression,
+  inScale
+) where
+
+import Data.List
+
+data Note = A | Bb | B | C | Cs | D | Eb | E | F | Fs | G | Ab
+  deriving (Eq, Ord)
+
+instance Show Note where
+  show A = "A"
+  show Bb = "Bb"
+  show B = "B"
+  show C = "C"
+  show Cs = "C#"
+  show D = "D"
+  show Eb = "Eb"
+  show E = "E"
+  show F = "F"
+  show Fs = "F#"
+  show G = "G"
+  show Ab = "Ab"
+
+notes = [C, Cs, D, Eb, E, F, Fs, G, Ab, A, Bb, B]
+
+fromNote :: Note -> Int
+fromNote = maybe 0 id . flip elemIndex notes
+
+toNote :: Int -> Note
+toNote n = notes !! (n `mod` 12)
+
+data ScalePosition = I | II | III | IV | V | VI | VII | VIII
+  deriving (Show, Eq, Ord)
+
+-- scalePositions = [I, II, III, IV, V, VI, VII, VIII]
+
+-- fromScalePosition :: ScalePosition -> Int
+-- fromScalePosition = maybe 1 id . flip elemIndex scalePositions
+
+-- toScalePosition :: Int -> ScalePosition
+-- toScalePosition n = scalePositions !! (n `mod` 7)
+
+data ScaleType = Major | Minor | Diminished | WholeTone
+  deriving (Show, Eq, Ord)
+
+-- scaleTypes = [Major, Minor, Diminished, WholeTone]
+
+flatten :: Note -> Note
+flatten = toNote . subtract 1 . fromNote
+
+-- sharpen :: Note -> Note
+-- sharpen = toNote . (+ 1) . fromNote
+
+scaleIntervals :: ScaleType -> [Int]
+scaleIntervals Major =      [2, 2, 1, 2, 2, 2]
+scaleIntervals Minor =      [2, 1, 2, 2, 2, 2]
+scaleIntervals Diminished = [1, 2, 1, 2, 1, 2, 1]
+scaleIntervals WholeTone =  [2, 2, 2, 2, 2]
+
+data Scale = Scale Note ScaleType
+  deriving (Eq, Ord)
+
+instance Show Scale where
+  show scale = case scale of
+      (Scale root Diminished) -> show' Diminished $ expandRoots 3 root
+      (Scale root WholeTone)  -> show' WholeTone $ expandRoots 2 root
+      (Scale root stype)      -> show' stype [root]
+    where 
+      show' :: ScaleType -> [Note] -> String
+      show' stype roots = unwords $ [intercalate "/" . map show $ roots, show stype]
+
+      expandRoots :: Int -> Note -> [Note]
+      expandRoots step = map toNote . take (12 `div` step) . iterate (+ step) . fromNote
+
+scales = [Scale root stype | stype <- [Major, Minor], root <- notes]
+      ++ [Scale A Diminished, Scale Bb Diminished, Scale B Diminished]
+      ++ [Scale A WholeTone, Scale Bb WholeTone]
+
+scaleNotes' :: ScaleType -> Note -> [Note]
+scaleNotes' stype root = map toNote $ scanl (+) (fromNote root) (scaleIntervals stype)
+
+scaleNotes :: Scale -> [Note]
+scaleNotes (Scale root stype) = scaleNotes' stype root
+
+scaleNote :: Int -> Note -> Note
+scaleNote p root = scaleNotes' Major root !! p
+
+inScale :: Scale -> Note -> Bool
+inScale s n = n `elem` (scaleNotes s)
+
+i = scaleNote 0
+-- ii = scaleNote 1
+iii = scaleNote 2
+-- iv = scaleNote 3
+v = scaleNote 4
+vi = scaleNote 5
+vii = scaleNote 6
+ix = scaleNote 8
+xi = scaleNote 10
+xiii = scaleNote 12
+
+fl :: (Note -> Note) -> (Note -> Note)
+fl = (flatten .)
+
+-- sh :: (Note -> Note) -> (Note -> Note)
+-- sh = (sharpen .)
+
+data ChordType = Maj | Maj7 | Dom7 | Dom7b5
+               | Min | Min7 | MinMaj7 | Min7b5
+               | Alt
+               | Dim
+
+instance Show ChordType where
+  show Maj = ""
+  show Maj7 = "Maj7"
+  show Dom7 = "7"
+  show Dom7b5 = "7b5"
+  show Min = "m"
+  show Min7 = "m7"
+  show MinMaj7 = "minMaj7"
+  show Alt = "alt"
+  show Dim = "o"
+  show Min7b5 = "m7b5"
+
+chordScalePositions :: ChordType -> [Note -> Note]
+chordScalePositions Maj = [i, iii, v]
+chordScalePositions Maj7 = [i, iii, v, vii]
+chordScalePositions Dom7 = [i, iii, v, fl vii]
+chordScalePositions Dom7b5 = [i, iii, fl v, fl vii]
+chordScalePositions Min = [i, fl iii, v]
+chordScalePositions Min7 = [i, fl iii, v, fl vii]
+chordScalePositions MinMaj7 = [i, fl iii, v, vii]
+chordScalePositions Min7b5 = [i, fl iii, fl v, fl vii]
+chordScalePositions Alt = [i, fl iii, fl v, fl vii, fl ix, fl xi, fl xiii]
+chordScalePositions Dim = [i, fl iii, fl v, vi]
+
+chordTypes = [Maj, Min, Maj7, Dom7, Min7, MinMaj7, Alt, Dim]
+
+data Chord = Chord Note ChordType
+
+instance Show Chord where
+  show (Chord n ct) = show n ++ show ct
+
+chordNotes' :: ChordType -> Note -> [Note]
+chordNotes' ct root = map ($ root) $ chordScalePositions ct
+
+chordNotes :: Chord -> [Note]
+chordNotes (Chord root ct) = chordNotes' ct root
+
+chordsForNotes :: [Note] -> [Chord]
+chordsForNotes ns = [Chord root ct | ct <- chordTypes, root <- notes, ns `containedIn` (chordNotes' ct root)]
+  where containedIn as bs = sort (as `intersect` bs) == sort as
+
+scalesForChord :: Chord -> [Scale]
+scalesForChord c = [s | s <- scales, (chordNotes c) `containedIn` (scaleNotes s)] 
+  where containedIn as bs = sort (as `intersect` bs) == sort as
+
+commonNotes :: Scale -> Scale -> [Note]
+commonNotes a b = sort $ scaleNotes a `intersect` scaleNotes b
+
+scalesForProgression :: Chord -> Chord -> [(Scale, Scale, [Note])]
+scalesForProgression a b = reverse $ sortOn (\(_, _, n) -> length n) [(sa, sb, commonNotes sa sb) | sa <- scalesForChord a, sb <- scalesForChord b]
